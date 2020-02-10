56a59b86e887eeeb6a5e7f813df5f1b964866914
Jan Mouwes <jan.mouwes@gmail.com>
2020-02-10 20:24:16 +0100

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const ROOT = __dirname;
+const SRC = path.join(ROOT, 'src');
+
+const output = path.join(ROOT, './dist');
+const entryPath = path.join(SRC, 'app.ts');
+
+module.exports = {
+    target: 'node',
+    entry: entryPath,
+    output: {
+        path: output,
+        filename: 'index.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(jsx?|tsx?)$/,
+                use: ['babel-loader'],
+                exclude: path.resolve(ROOT, 'node_modules')
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    }
+};
